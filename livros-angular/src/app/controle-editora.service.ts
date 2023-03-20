bd9c176727da5a4dd7771829351c14b9104f7ac8
Gilson Miranda Neto <34845199+gilsonmneto@users.noreply.github.com>
2023-03-20 08:20:54 -0300

    update

--- livros-angular/src/app/controle-editora.service.ts
+++ livros-angular/src/app/controle-editora.service.ts
@@ -6,9 +6,9 @@ import { Editora } from './editora';
 })
 export class ControleEditoraService {
   editoras: Array<Editora> = [
-    { codEditora: 1, nome: 'Alta Books' },
-    { codEditora: 2, nome: 'Pearson' },
-    { codEditora: 3, nome: 'Editora C' }
+    { codEditora: 1, nome: 'Editora 1' },
+    { codEditora: 2, nome: 'Editora 2' },
+    { codEditora: 3, nome: 'Editora  3' }
   ];
   constructor() { }
 
